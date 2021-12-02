95fdf694ac963b478afff830f4684ee69f8eba0f
Linus Marton <linus.pettersson@gmail.com>
2021-12-02 23:50:08 +0100

    Cleanup Part 01

--- day02/part01.rb
+++ day02/part01.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-puts File.foreach("./input.txt").each_with_object(Hash.new(0)) { |l, hsh| dir, dist = l.chomp.split; hsh[dir] += dist.to_i }.then { |res| res["forward"] * (res["down"] - res["up"]) }
+p = File.foreach("input.txt").map(&:split).each_with_object(Hash.new(0)) { |(dir, dist), hash| hash[dir] += dist.to_i }
+puts (p["down"] - p["up"]) * p["forward"]
