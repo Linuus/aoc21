60e538cef0a649711d454814144823a7b0cda264
Linus Marton <linus.pettersson@gmail.com>
2021-12-03 14:40:14 +0100

    Day 2 Part 2

--- day02/part02.rb
+++ day02/part02.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
-p = File.foreach("input.txt").map(&:split).each_with_object(Hash.new(0)) { |(dir, dist), hash| hash[dir] += dist.to_i }
+p = File.foreach("input.txt").map(&:split).each_with_object(Hash.new(0)) do |(dir, dist), hash|
+  case dir
+  when "up"
+    hash["aim"] -= dist.to_i
+  when "down"
+    hash["aim"] += dist.to_i
+  when "forward"
+    hash["forward"] += dist.to_i
+    hash["depth"] += hash["aim"] * dist.to_i
+  end
+end
 
-puts (p["down"] - p["up"]) * p["forward"]
+puts p["depth"] * p["forward"]
